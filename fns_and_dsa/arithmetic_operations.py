27e1df88e959a99ba34bf4ad051cebe8c56bc30f
Judith Ogbole <judithogbole71@gmail.com>
2024-09-29 19:33:19 +0100

    task 0

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation):
+
+
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num2 - num1
+    elif operation == "multiply":
+        result = num1 * num2
+    else:
+        if num2 == 0:
+            print("Error: Cannot be divided by Zero")
+        else:
+            result = num1 / num2
+
+    return(result)
