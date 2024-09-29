5d2e11fc3e5705603e0c439149bb9b474d49f157
Judith Ogbole <judithogbole71@gmail.com>
2024-09-29 20:14:29 +0100

    task 1 done

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,35 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            add = input("Add an Item: ")
+            shopping_list.append(add)
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            delete = input("Remove an Item: ")
+            shopping_list.remove(delete)
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
