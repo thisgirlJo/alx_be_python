9f8f795d6e8373227f3dedc992dbda66be347724
Judith Ogbole <judithogbole71@gmail.com>
2024-09-29 23:57:44 +0100

    arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,7 +4,7 @@ def perform_operation(num1, num2, operation):
     if operation == "add":
         result = num1 + num2
     elif operation == "subtract":
-        result = num2 - num1
+        result = num1 - num2
     elif operation == "multiply":
         result = num1 * num2
     else:
