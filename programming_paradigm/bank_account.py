c247d085e88aa48acded198ea1f1f54579731a25
Judith Ogbole <judithogbole71@gmail.com>
2024-10-02 13:42:39 +0100

    Bank Account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, account_balance, deposit, withdraw, display_balance):
+        self.account_balance = account_balance
+        self.deposit = deposit
+        self.withdraw = withdraw
+        self.display_balance = display_balance
+    
+    def deposit(self, amount):
+        amount = float(input("Enter Amount to be Deposited: "))
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(self, amount):
+        amount = float(input("Enter Amount to be Deposited: "))
+        self.account_balance -= amount
+        return self.account_balance
+
+    def display_balance(self):
+        return f"Current Balance: $", {self.account_balance}
+    
