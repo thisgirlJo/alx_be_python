f8a2010fe158f72592888c70e63f8dd9af11016e
thisgirlJo <judithogbole71@gmail.com>
2024-10-04 20:55:13 +0100

    Bank Account-Task 0

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,10 +1,10 @@
+#Understand the fundamentals of OOP in Python by 
+#implementing a BankAccount class that encapsulates banking operations.
+#Use command line arguments to interact with instances of this class.
+
 class BankAccount:
-    def __init__(self, account_balance, deposit, withdraw, display_balance):
-        super().__init__(deposit, withdraw, display_balance)
-        self.account_balance = 0
-        self.deposit = deposit
-        self.withdraw = withdraw
-        self.display_balance = display_balance
+    def __init__(self):
+        self.account_balance = 0 #Default Attribute Value
     
     def deposit(self, amount):
         amount = float(input("Enter Amount to be Deposited: "))
