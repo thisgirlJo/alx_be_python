7c3475d5fd74e55b953e50d95e273ac8c3e0a176
Judith Ogbole <judithogbole71@gmail.com>
2024-10-06 12:01:31 +0100

    Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,17 +2,17 @@
 
 class BankAccount:
     def __init__(self, account_balance):
-        self.account_balance = account_balance #Default Attribute Value
-    
+        self.account_balance = 0
+
     def deposit(self, amount):
-        self.account_balance += amount
+        self.account_balance = self.account_balance + amount
+        return self.account_balance
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
-            self.account_balance = self.account_balance - amount
-            return self.account_balance
+            return True
         else:
-            return self.account_balance
+            return False
 
     def display_balance(self):
         print("Current Balance: $",self.account_balance)
