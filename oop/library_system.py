0b5609370a4bfdb73dbff5e534815f0b2492a7b5
Judith Ogbole <judithogbole71@gmail.com>
2024-10-26 12:19:51 +0100

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,37 +3,34 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        print(f"Book: {self.title} by {self.author}")
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size: int):
-        self.file_size = file_size
         super().__init__(title, author)
+        self.file_size = file_size
     
     def __str__(self):
-        return super().__str__()
-        print(f"EBOOK: {self.title} by {self.author}, File Size: {self.file_size}")
+        super().__repr__()
+        return f"EBOOK: {self.title} by {self.author}, File Size: {self.file_size}"
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count: int):
-        self.page_count = page_count
         super().__init__(title, author)
+        self.page_count = page_count
     
     def __str__(self):
-        return super().__str__()
-        print(f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}")
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
-class Library:
+class Library(Book):
     def __init__(self):
-        self.books = []
+        self.books = [] #Create an empty List
 
     def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
-        if EBook:
-            return f"EBOOK: {self.title} by {self.author}, File Size: {self.file_size}"
-        elif PrintBook:
-            return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-        else:
-            return f"Book: {self.title} by {self.author}"
+        for x in self.books:
+            print(x)
