fab5170763cbd793acd26cecc16f5920a1c27ffe
Judith Ogbole <judithogbole71@gmail.com>
2024-10-29 01:03:00 +0100

    Library Management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -5,28 +5,36 @@ class Book:
         self.title = title
         self.author = author
         self._is_checked_out = False
+
     def check_out(self):
         if  not self._is_checked_out:
             return True
         else:
             return False
+        
 class Library(Book):
     def __init__(self):
-        #@classmethod
-        #super.__init__(title, author)
         self._books = []
+        self._return = []
 
     def add_book(self, book):
         self._books.append(book)
+        self._return.append(book)
+        #print(f"{self._books[0].author}")
+        #print(f"{self._books[0].title}: added successfully")
 
     def check_out_book(self, title):
         for book in self._books:
             if book.title == title:
                 if book.check_out():
-                    self._book.remove(book)
+                    self._books.remove(book)
+
+    def list_available_books(self):
+        for book in self._books:
+            print(f"{book.title} by {book.author}")
+        #return f"{} by {self._books.author}"
 
     def return_book(self, title):
-        pass
-    def list_available_books(self):
-        #print(f"{self.title} by {self.author}")
-        pass
+        for book in self._return:
+            if book.title == title:
+                self._books.append(book)
